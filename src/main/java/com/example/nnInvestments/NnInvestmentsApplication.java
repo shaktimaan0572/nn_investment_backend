043406e9dc78f61a5b53a705c6f866b294b10525
vivekMajumdar <vivek.majumdar@99acres.com>
2022-09-28 13:29:32 +0530

    MERGE | fix

--- src/main/java/com/example/nnInvestments/NnInvestmentsApplication.java
+++ src/main/java/com/example/nnInvestments/NnInvestmentsApplication.java
@@ -3,8 +3,13 @@ package com.example.nnInvestments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class NnInvestmentsApplication {
@@ -13,16 +18,30 @@ public class NnInvestmentsApplication {
 		SpringApplication.run(NnInvestmentsApplication.class, args);
 	}
 
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**")
+//						.allowedOrigins("https://nn-investment-service.herokuapp.com/")
+//						.allowedMethods("GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD");
+//			}
+//		};
+//	}
+
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("https://nn-investment-service.herokuapp.com/")
-						.allowedMethods("GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD");
-			}
-		};
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("https://nn-investment-service.herokuapp.com/"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type"));
+		configuration.setExposedHeaders(Arrays.asList("X-Get-Header"));
+		configuration.setMaxAge(3600L);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 }
