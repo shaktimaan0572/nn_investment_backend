31f9491f903f258bee1ed28eae653bbcdc7a468b
vivekMajumdar <vivek.majumdar@99acres.com>
2022-09-28 13:09:39 +0530

    MERGE | cors filter added

--- src/main/java/com/example/nnInvestments/NnInvestmentsApplication.java
+++ src/main/java/com/example/nnInvestments/NnInvestmentsApplication.java
@@ -2,12 +2,25 @@ package com.example.nnInvestments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NnInvestmentsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NnInvestmentsApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://nn-investment-service.herokuapp.com/");
+			}
+		};
 	}
 
 }
